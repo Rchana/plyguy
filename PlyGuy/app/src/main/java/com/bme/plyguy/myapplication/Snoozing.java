954ee84b84839ed600a86c89046ac15d5dea112a
Laura <laura@Lauras-MacBook-Air.local>
2018-03-25 00:26:03 -0400

    other UI pages

---
+++ PlyGuy/app/src/main/java/com/bme/plyguy/myapplication/Snoozing.java
@@ -0,0 +1,13 @@
+package com.bme.plyguy.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Snoozing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_snoozing);
+    }
+}
